f573d5d1705c30ea54ad8365e61e1b4894ea6dd9
sangwoonoel <sangwoonoel@cau.ac.kr>
2024-01-16 14:54:28 +0000

    feat: attnlstm 모델 구현 #21

---
+++ dkt/dkt/attnlstm/attnlstm.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+from transformers.models.bert.modeling_bert import BertConfig, BertEncoder
+from ..model import ModelBase
+
+
+class ATTNLSTM(ModelBase):
+    def __init__(
+        self,
+        hidden_dim: int = 64,
+        n_layers: int = 2,
+        n_tests: int = 1538,
+        n_questions: int = 9455,
+        n_tags: int = 913,
+        n_heads: int = 2,
+        drop_out: float = 0.1,
+        **kwargs
+    ):
+        super().__init__(
+            hidden_dim,
+            n_layers,
+            n_tests,
+            n_questions,
+            n_tags
+        )
+        self.n_heads = n_heads
+        self.drop_out = drop_out
+
+        self.config = BertConfig(
+            3,  # not used
+            hidden_size=self.hidden_dim,
+            num_hidden_layers=1,
+            num_attention_heads=self.n_heads,
+            intermediate_size=self.hidden_dim,
+            hidden_dropout_prob=self.drop_out,
+            attention_probs_dropout_prob=self.drop_out,
+        )
+        self.attn = BertEncoder(self.config)
+
+        self.lstm = nn.LSTM(
+            self.hidden_dim, self.hidden_dim, self.n_layers, batch_first=True
+        )
+
+    def forward(self, test, question, tag, correct, mask, interaction):
+        X, batch_size = super().forward(test=test,
+                                        question=question,
+                                        tag=tag,
+                                        correct=correct,
+                                        mask=mask,
+                                        interaction=interaction)
+        extended_attention_mask = mask.unsqueeze(1).unsqueeze(2)
+        extended_attention_mask = extended_attention_mask.to(dtype=torch.float32)
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+        head_mask = [None] * self.n_layers
+
+        encoded_layers = self.attn(X, extended_attention_mask, head_mask=head_mask)
+        sequence_output = encoded_layers[-1]
+        out, _ = self.lstm(sequence_output)
+        out = out.contiguous().view(batch_size, -1, self.hidden_dim)
+
+        out = self.fc(out).view(batch_size, -1)
+        return out
+    
