3570ee27b9f536f64d0ffc15b311884468466033
GangBean <xbox2537@gmail.com>
2024-01-19 06:34:30 +0000

    current_correct_count

--- dkt/dkt/model.py
+++ dkt/dkt/model.py
@@ -49,7 +49,7 @@ class ModelBase(nn.Module):
         self.embedding_guess = nn.Embedding(3, guess_dim)
 
         # Concatentaed Embedding Projection3
-        features_len = (intd * 4) + 1 + (test_group_dim * 2) + serial_dim + (tag_group_dim * 1) + 8 + guess_dim
+        features_len = (intd * 4) + 1 + (test_group_dim * 2) + serial_dim + (tag_group_dim * 1) + 9 + guess_dim
         
         self.comb_proj = nn.Linear(features_len, hd)
 
@@ -128,7 +128,7 @@ class ModelBase(nn.Module):
                 same_tag_correct_before.unsqueeze(-1).int(),
                 same_tag_wrong_before.unsqueeze(-1).int(),
                 #embed_correct_percent,
-                #current_correct_count.unsqueeze(-1).int(),
+                current_correct_count.unsqueeze(-1).int(),
                 #embed_tag_group_one,
                 embed_tag_group_two,
                 time_for_solve.unsqueeze(-1).int(),
