9f7eb82593ca75ce0089adb5a09daa8db6351336
Hyunjoo Lee <alro92333@gmail.com>
2024-01-24 17:49:53 +0900

    [FEAT] LightGCN 의 학습 결과 저장을 위한 코드 변경 + LR Scheduler 을 이용한 sweep  (#43)
    * feat: log values for lightgcn
    
    * Update sweep
    
    * fix typo and comment
    
    * fix comment
    
    * Update lightgcn for valid split
    
    * Update lightgcn for valid split

--- lightgcn/lightgcn/scheduler.py
+++ lightgcn/lightgcn/scheduler.py
@@ -1,18 +1,11 @@
 import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau, LambdaLR, StepLR, CosineAnnealingLR
-from transformers import get_linear_schedule_with_warmup
-
 
 def get_scheduler(optimizer: torch.optim.Optimizer, args):
+    scheduler = None
     if args.scheduler == "plateau":
         scheduler = ReduceLROnPlateau(
-            optimizer, patience=10, factor=0.5, mode="max", verbose=True
-        )
-    elif args.scheduler == "linear_warmup":
-        scheduler = get_linear_schedule_with_warmup(
-            optimizer,
-            num_warmup_steps=args.warmup_steps,
-            num_training_steps=args.total_steps,
+            optimizer, patience=10, factor=0.5, mode="min", verbose=True
         )
     elif args.scheduler == "lambda":
         scheduler = LambdaLR(optimizer, lr_lambda = lambda epoch: 0.95 ** epoch)
