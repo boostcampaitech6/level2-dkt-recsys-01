32cffc03a26f82587af7391950ece4993e3c302d
Hyunjoo Lee <alro92333@gmail.com>
2024-01-25 08:09:35 +0000

    Update valid split (wrong)

--- lightgcn/lightgcn/trainer.py
+++ lightgcn/lightgcn/trainer.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import torch
 from torch import nn
+from copy import deepcopy
 from easydict import EasyDict
 from torch_geometric.nn.models import LightGCN
 from sklearn.metrics import accuracy_score, roc_auc_score
@@ -68,7 +69,7 @@ def run(
                        train_acc = train_acc,
                        train_auc = train_auc,
                        valid_acc = valid_acc,
-                       valid_auc = valid_auc,
+                       valid_auc =valid_auc,
                        train_confusion_matrix = wandb_train_cf,
                        valid_confusion_matrix = wandb_valid_cf))
 
@@ -84,6 +85,57 @@ def run(
             
     torch.save(obj={"model": model.state_dict(), "epoch": e + 1}, f=os.path.join(f'{args.model_dir}{args.run_name}/', f"{args.run_name}_last_model.pt"))
     logger.info(f"Best Weight Confirmed : {best_epoch+1}'th epoch")
+    
+def run2(
+    model: nn.Module,
+    train_edges,
+    train_edge_labels,
+    valid_edges,
+    valid_edge_labels,
+    args: EasyDict
+):
+    model.train()
+    optimizer = get_optimizer(model=model, args=args)
+    scheduler = get_scheduler(optimizer=optimizer, args=args)
+    
+    patience = 5
+    least_loss, num = 1e+9, 0
+    os.makedirs(name=f'{args.model_dir}{args.run_name}/', exist_ok=True)
+
+    for e in range(args.n_epochs):
+        # train
+        train_loss, train_auroc, train_accuracy = train2(optimizer, model, train_edges, train_edge_labels)
+        # valid
+        valid_loss, valid_auroc, valid_accuracy = validate2(optimizer, model, valid_edges, valid_edge_labels)
+        #print(f"epochs {e}: train loss: {train_loss}, train auroc: {train_auroc:.4f}, train accuracy: {train_accuracy:.4f}")
+        #print(f"            valid loss: {valid_loss}, valid auroc: {valid_auroc:.4f}, valid accuracy: {valid_accuracy:.4f}")
+        
+        if valid_loss < least_loss:
+            print(f'minimum valid loss is {valid_loss:.4f} at {e} epoch')
+            least_loss, num = valid_loss, 0
+            best_auc, best_epochs = valid_auroc, e
+            best_model = deepcopy(model.state_dict())
+        else:
+            num += 1
+            if num >= patience:
+                print(f'early stopped at {e} epoch')
+                break
+        wandb.log(dict(train_loss = train_loss,
+                       valid_loss = valid_loss,
+                       train_acc = train_accuracy,
+                       train_auc = train_auroc,
+                       valid_acc = valid_accuracy,
+                       valid_auc =valid_auroc))
+
+        if valid_auroc > best_auc:
+            logger.info("Best model updated AUC from %.4f to %.4f", best_auc, valid_auroc)
+            best_auc, best_epoch = valid_auroc, e
+            torch.save(obj= {"model": model.state_dict(), "epoch": e + 1}, f=os.path.join(f'{args.model_dir}{args.run_name}/', f"{args.run_name}_best_model.pt"))
+        # 1번
+        if args.scheduler == "plateau":
+            scheduler.step(best_auc)
+        else:
+            scheduler.step()
     
 
 def train(model: nn.Module, train_data: dict, optimizer: torch.optim.Optimizer,  scheduler:torch.optim.lr_scheduler._LRScheduler, args: EasyDict):
@@ -129,6 +181,38 @@ def validate(valid_data: dict, model: nn.Module):
 
     return valid_acc, valid_auc, valid_loss, wandb_valid_cf
 
+def train2(optimizer, model, edge_index, edge_labels):
+    model.train()
+
+    preds = model.predict_link(edge_index, prob=True)
+    
+    optimizer.zero_grad()
+    loss = model.link_pred_loss(preds, edge_labels)
+    loss.backward()
+    optimizer.step()
+
+    preds_ = preds.detach().cpu().numpy()
+    labels = edge_labels.detach().cpu().numpy()
+    
+    auroc = roc_auc_score(labels, preds_)
+    accuracy = accuracy_score(labels, preds_ > 0.5)
+    logger.info("TRAIN ACC : %.4f AUC : %.4f LOSS : %.4f", accuracy, auroc, loss.item())
+    return loss, auroc, accuracy
+
+def validate2(optimizer, model, edge_index, edge_labels):
+    model.eval()
+
+    preds = model.predict_link(edge_index, prob=True)
+    loss = model.link_pred_loss(preds, edge_labels)
+
+    preds_ = preds.detach().cpu().numpy()
+    labels = edge_labels.detach().cpu().numpy()
+
+    auroc = roc_auc_score(labels, preds_)
+    accuracy = accuracy_score(labels, preds_>0.5)
+    logger.info("VALID ACC : %.4f AUC : %.4f LOSS : %.4f", accuracy, auroc, loss.item())
+    return loss, auroc, accuracy
+
 
 def inference(model: nn.Module, data: dict, args : EasyDict):
     model.eval()
@@ -140,10 +224,5 @@ def inference(model: nn.Module, data: dict, args : EasyDict):
     os.makedirs(name=args.output_dir, exist_ok=True)
     last_write_path = os.path.join(args.output_dir, f"{args.run_name}_last_submission.csv")
     pd.DataFrame({"prediction": pred}).to_csv(path_or_buf=last_write_path, index_label="id")
-    
-    best_model = torch.load(os.path.join(f'{args.model_dir}{args.run_name}/', f"{args.run_name}_best_model.pt"))
-    best_write_path = os.path.join(args.output_dir, f"{args.run_name}_best_submission.csv")
-    pd.DataFrame({"prediction": pred}).to_csv(path_or_buf=best_write_path, index_label="id")
-    
+
     logger.info("Successfully saved submission as %s", last_write_path)
-    logger.info("Successfully saved submission as %s", best_write_path)
